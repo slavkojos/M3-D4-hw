ae262649a0bf02d2d27b5a3a3b45338469ff0f7a
slavko <slavkoj6@gmail.com>
2021-03-12 00:40:28 +0100

    implement adding to cart and displaying cart

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ function fetchUrl(event, endpoint) {
     });
 }
 
+const showCartButton = document.getElementById("load-cart");
+showCartButton.onclick = showCart;
+showCartButton.setAttribute("data-toggle", "modal");
+showCartButton.setAttribute("data-target", "#exampleModal");
+
 const loadBooksButton = document.getElementById("load-books");
 loadBooksButton.addEventListener("click", fetchUrl);
 mainRow = document.getElementById("main-row");
@@ -54,11 +59,8 @@ function loadCards() {
     buttonsDiv.classList.add("d-flex", "justify-content-center", "p-4");
     card.appendChild(buttonsDiv);
     const addToCartButton = document.createElement("button");
-    /* addToCartButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="mr-1" fill="none" viewBox="0 0 24 24" style="height:25px" stroke="currentColor">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-  </svg> Add to cart`; */
     const cartSymbol = document.createElement("i");
-    cartSymbol.classList.add("fas", "fa-shopping-cart");
+    cartSymbol.classList.add("fas", "fa-shopping-cart", "mr-1");
     addToCartButton.appendChild(cartSymbol);
     const addToCartSpan = document.createElement("span");
     addToCartButton.appendChild(addToCartSpan);
@@ -82,28 +84,106 @@ function showModal(event) {
 let cartItems = [];
 
 function addToCart(event) {
-  console.log(event);
-  let addToCartButton = event.target;
+  console.log(event.currentTarget.parentNode.parentNode);
+  let addToCartButton = event.currentTarget;
   addToCartButton.classList.remove("btn-info");
   addToCartButton.classList.add("btn-success");
-  addToCartButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" style="height:25px" class="mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-</svg> Added! `;
+  addToCartButton.innerHTML = `<i class="fas fa-check mr-1"></i> Added!`;
   addToCartButton.removeEventListener("click", addToCart);
   addToCartButton.addEventListener("click", removeFromCart);
+  const cards = document.getElementsByClassName("card");
+  cartItems.push(
+    parseInt(
+      Array.from(event.currentTarget.parentNode.parentNode.parentNode.parentNode.children).indexOf(
+        event.currentTarget.parentNode.parentNode.parentNode
+      )
+    )
+  );
+  console.log(cartItems);
 }
 
 function removeFromCart(event) {
-  let addToCartButton = event.target;
+  let addToCartButton = event.currentTarget;
   addToCartButton.classList.remove("btn-success");
   addToCartButton.classList.add("btn-info");
-  addToCartButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="mr-1" fill="none" viewBox="0 0 24 24" style="height:25px" stroke="currentColor">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-  </svg> Add to cart `;
+  addToCartButton.innerHTML = `<i class="fas fa-shopping-cart mr-1"></i> Add to cart`;
   addToCartButton.removeEventListener("click", removeFromCart);
   addToCartButton.addEventListener("click", addToCart);
+  cartItems.splice(
+    cartItems.indexOf(
+      Array.from(event.currentTarget.parentNode.parentNode.parentNode.parentNode.children).indexOf(
+        event.currentTarget.parentNode.parentNode.parentNode
+      )
+    ),
+    1
+  );
+  console.log(cartItems);
 }
 
-function hideCard(event) {
-  event.target.parentNode.parentNode.classList.add("d-none");
+const modalBody = document.getElementById("modal-body");
+function showCart() {
+  if (cartItems.length === 0) {
+    console.log("okk");
+    modalBody.innerHTML = `<h3 class="text-center">Cart is empty</h3>`;
+  } else loadCart();
+}
+
+function loadCart() {
+  modalBody.innerHTML = "";
+  const cartTable = document.createElement("table");
+  cartTable.classList.add("w-100");
+  const cartTableHead = document.createElement("thead");
+  const cartTableHeadRow = document.createElement("tr");
+  const cartTableHeader1 = document.createElement("th");
+  const cartTableHeader2 = document.createElement("th");
+  const cartTableHeader3 = document.createElement("th");
+  const cartTableHeader4 = document.createElement("th");
+  const cartTableBody = document.createElement("tbody");
+  modalBody.appendChild(cartTable);
+  cartTable.appendChild(cartTableHead);
+  cartTableHead.appendChild(cartTableHeadRow);
+  cartTableHeadRow.appendChild(cartTableHeader1);
+  cartTableHeadRow.appendChild(cartTableHeader2);
+  cartTableHeadRow.appendChild(cartTableHeader3);
+  cartTableHeadRow.appendChild(cartTableHeader4);
+  cartTableHeader1.setAttribute("scope", "col");
+  cartTableHeader2.setAttribute("scope", "col");
+  cartTableHeader3.setAttribute("scope", "col");
+  cartTableHeader4.setAttribute("scope", "col");
+  cartTable.appendChild(cartTableBody);
+  cartTableHeader1.innerText = "#";
+  cartTableHeader2.innerText = "Image";
+  cartTableHeader3.innerText = "Title";
+  cartTableHeader4.innerText = "Price";
+  let tableRowID = 0;
+  let totalPrice = 0;
+  cartItems.map((cartItem) => {
+    tableRowID++;
+    console.log(booksData[cartItem].title);
+    const cartTableBodyRow = document.createElement("tr");
+    cartTableBodyRow.classList.add("my-4");
+    const cartItemID = document.createElement("td");
+    const cartItemImage = document.createElement("td");
+    const cartItemPicture = document.createElement("img");
+    cartItemImage.appendChild(cartItemPicture);
+    const cartItemTitle = document.createElement("td");
+    const cartItemPrice = document.createElement("td");
+    cartTableBody.appendChild(cartTableBodyRow);
+    cartTableBodyRow.appendChild(cartItemID);
+    cartTableBodyRow.appendChild(cartItemImage);
+    cartTableBodyRow.appendChild(cartItemTitle);
+    cartTableBodyRow.appendChild(cartItemPrice);
+    cartItemID.innerText = tableRowID;
+    cartItemPicture.setAttribute("src", `${booksData[cartItem].img}`);
+    cartItemPicture.style.height = "100px";
+    cartItemTitle.innerText = booksData[cartItem].title;
+    cartItemPrice.innerText = `${booksData[cartItem].price} €`;
+    totalPrice += parseFloat(booksData[cartItem].price);
+  });
+  const total = document.createElement("h5");
+  const hr = document.createElement("hr");
+  modalBody.appendChild(hr);
+  modalBody.appendChild(total);
+  total.classList.add("text-right", "pr-3");
+  total.innerText = `TOTAL: ${totalPrice.toFixed(2)} €`;
 }
